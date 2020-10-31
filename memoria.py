381ccfdb713333c99d3846eb2840603d486066ca
IsaacArredondoPadron <73501289+IsaacArredondoPadron@users.noreply.github.com>
2020-10-31 00:05:52 -0600

    Agregue el codigo para detectar cuando se han volteado todas las cartas

--- memoria.py
+++ memoria.py
@@ -2,6 +2,7 @@
 from random import *
 from turtle import *
 from freegames import path
+import time
 
 # Integrantes del equipo:
 # Juan Angel Mora Moreno | A00517141
@@ -10,6 +11,7 @@ from freegames import path
 car = path('car.gif')
 tiles = list(range(32)) * 2
 state = {'mark': None}
+contador = {'tarjetas': 0}
 hide = [True] * 64
 
 def square(x, y):
@@ -48,6 +50,7 @@ def tap(x, y):
         hide[spot] = False
         hide[mark] = False
         state['mark'] = None
+        contador['tarjetas'] += 1
 
 def draw():
     "Draw image and tiles."
@@ -72,6 +75,26 @@ def draw():
 
     update()
     ontimer(draw, 100)
+    # Aqui detectamos cuando todos los cuadros se han volteado
+    if contador['tarjetas'] == 32:
+      color("blue")
+      begin_fill()
+      goto(-200,0)
+      forward(400)
+      left(90)
+      forward(50)
+      left(90)
+      forward(400)
+      left(90)
+      forward(50)
+      left(90)
+      end_fill()
+      goto(195,10)
+      color("yellow")
+      write("!Felicidades has ganado¡", False,
+          "right", ("arial", 20, "bold italic"))
+      time.sleep(5)
+      exit()
 
 # Inicializador de los taps ejecutados
 taps = 0 
